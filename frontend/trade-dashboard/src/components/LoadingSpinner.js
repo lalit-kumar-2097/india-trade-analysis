29713fad95cca651c29e46bbd0f99e017cbb55ff
Lalit Kumar <37204205+lalit-kumar-2097@users.noreply.github.com>
2025-07-17 14:34:05 +0530

    Delete frontend/trade-dashboard directory

--- frontend/trade-dashboard/src/components/LoadingSpinner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center my-10">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
-    </div>
-  );
-}
